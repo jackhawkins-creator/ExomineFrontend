fa4fd56b9ddced99656591fc325046313d8e95c7
Cwelshofer1 <sweetbrooo@gmail.com>
2025-02-13 14:42:43 -0600

    created main.js

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+
+const container = document.querySelector("")
+
+const render = async () => {
+
+    const exomineHTML = `
+    <h1>Solar System Marketplace</h1>
+
+        <article class="choices">
+            <section class="govenors_options">
+
+            </section>
+
+            <section class="facilities_options">
+
+            </section>
+
+            <section class="minerals_options">
+                
+            </section>
+
+            <section class="colonyMinerals">
+                
+            </section>
+        </article>
+
+        <article class="cart">
+
+            <section class="spaceCart">
+            <h2>Space Cart</h2>
+            
+            </section>
+
+            <section class="purchaseButton">
+                      
+            </section>
+        </article>
+    `
+container.innerHTML = exomineHTML
+}
